2cc83f8ee7a651c1befee671be7c31469d31e8f3
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2023-07-13 12:00:03 -0300

    creating addToDo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Header } from './components/Header/Header'
+import styles from "./App.module.css"
+import plusIcon from "./assets/plus.svg"
 import './global.css'
 
 function App() {
   return (
     <>
 		<Header/>
+		<div className={styles.wrapper}>
+			<div className={styles.addToDo}>
+				<input type="text" placeholder='Adicione uma nova tarefa' />
+				<button>Criar <img src={plusIcon} /></button>
+			</div>
+		</div>
     </>
   )
 }
